135c44793760686536c89f8e41a5805c075ff7f6
ZJ <zak@newalexandria.org>
2022-07-30 19:20:56 -0400

    tests: add a harnet for testing the API via the console

--- lib/solidus_shipstation/console_harness.rb
+++ lib/solidus_shipstation/console_harness.rb
@@ -16,20 +16,35 @@ module SolidusShipstation
       @shipments = SolidusShipstation::Api::ScheduleShipmentSyncsJob.new.query_shipments
     end
 
+    def shipment(id)
+      @shipments.find_by(id: id)
+    end
+
+    def shipment_number(ship_number)
+      @shipments.find_by(number: ship_number)
+    end
+
     def serialize(shipment)
       # SolidusShipstation::Api::ApplianceShipmentSerializer.new(shipment)
       @syncer.client.shipment_serializer.call(shipment)
     end
 
+    def try_one
+      puts "trying shipment #{(shipment = @shipments[@cursor]).id}"
+      resp = @runner.call(:post, '/orders/createorders', [serialize(shipment)])
+      unless resp['hasErrors']
+        @cursor += 1
+        return true
+      end
+    ensure
+      puts resp
+    end
+
     def try_batch(batch_size=nil)
       b = [batch_size.to_i, @batch].max
-      resps = @runner.call(:post, '/orders/createorders', @shipments[@cursor..@cursor+b])
-
-      # this is not a safe advance of the cursor since just some records could fail or succeed
-      # @cursor = @cursor+b  if resps.map{|r| r['hasErrors'] }.reduce(&:&) # if no errors
-
-      @cursor = @cursor+b
-      resps
+      b.times do
+        break unless try_one
+      end
     end
   end
 end
