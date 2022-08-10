fa9175689685291eff61e157627c0c1df5a2b44e
NewAlexandria <NewAlexandria@users.noreply.github.com>
2022-08-09 20:38:41 -0400

    (#1) Add a harness for testing the API via the console
    * move ScheduleShipmentSyncsJob records getter into dedicated method
    
    * add exploratory harness for console working
    
    * tests: add a harnet for testing the API via the console
    
    * make it easier to access the last response received by the runner
    
    * update README.  try_one can specify which one

---
+++ lib/solidus_shipstation/console_harness.rb
@@ -0,0 +1,51 @@
+module SolidusShipstation
+  class ConsoleHarness
+    attr_reader :runner, :syncer, :shipments
+
+    attr_accessor :cursor, :batch
+
+    def initialize
+      @runner = SolidusShipstation::Api::RequestRunner.from_config
+      @syncer = SolidusShipstation::Api::BatchSyncer.from_config
+      @sync = SolidusShipstation::Api::ScheduleShipmentSyncsJob.new
+      @shipments = @sync.query_shipments
+      @cursor = 0
+      @batch = 4
+    end
+
+    def refresh
+      @shipments = (@sync = SolidusShipstation::Api::ScheduleShipmentSyncsJob.new).query_shipments
+    end
+
+    def shipment(id)
+      @shipments.find_by(id: id)
+    end
+
+    def shipment_number(ship_number)
+      @shipments.find_by(number: ship_number)
+    end
+
+    def serialize(shipment)
+      # SolidusShipstation::Api::ApplianceShipmentSerializer.new(shipment)
+      @syncer.client.shipment_serializer.call(shipment)
+    end
+
+    def try_one(a_shipment=nil)
+      puts "trying shipment #{(shipment = a_shipment || @shipments[@cursor]).id}"
+      resp = @runner.call(:post, '/orders/createorders', [serialize(shipment)])
+      unless resp['hasErrors']
+        @cursor += 1 unless a_shipment
+        return true
+      end
+    ensure
+      puts resp
+    end
+
+    def try_batch(batch_size=nil)
+      b = [batch_size.to_i, @batch].max
+      b.times do
+        break unless try_one
+      end
+    end
+  end
+end
